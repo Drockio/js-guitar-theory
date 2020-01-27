8cf94d549378eb898edcce23d08685dd82ab11a7
Derek Dunnom <derek.dunnom@gmail.com>
2020-01-27 06:33:01 -0800

    refactoring

--- music.js
+++ music.js
@@ -7,7 +7,6 @@ function Music(strings, targetElement) {
     this.strings = strings.split(',');
     this.targetElement = targetElement;
     this.targetElementSelector = '#' + targetElement;
-    this.clearFretboard();
     this.populateStrings();
 }
 
@@ -16,32 +15,45 @@ Music.prototype = {
     numFrets : 20,
     notes: ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#'],
 
-    clearFretboard : function() {
-        $(this.targetElementSelector + ' ol').remove();
-    },
-
     removeHighlighting: function () {
         $(this.targetElementSelector + ' ol li span.highlight').removeClass('highlight');
     },
 
     updateFretboard: function () {
-        this.clearFretboard();
-        this.strings = ($('select#tuning option:selected').val()).split(',');
+        fretBoard.clearFretboard();
         var scale = $('select#scale option:selected').val();
         var key = $('select#key option:selected').val();
-        
+
+        //setup strings 
+        this.strings = ($('select#tuning option:selected').val()).split(',');
         this.populateStrings();
         switch (scale) {
             case 'major-scale':
-                this.showMajorScale(key);
+                musicScales.showMajorScale(key);
                 break;
             case 'pentatonic-scale':
-                this.showPentatonicScale(key);
+                musicScales.showPentatonicScale(key);
                 break;
             case 'major-chord-notes':
-                this.showMajorChordNotes(key);
+                musicScales.showMajorChordNotes(key);
                 break;
         }
+    },
+
+    getNoteViaInterval : function(note, interval) {
+        var index = this.notes.indexOf(note);
+
+        var targetNote;
+        //interval goes off the deep end
+        if ((interval + index) >= this.notes.length) {
+            var initialNotes = this.notes.length - index;
+            targetNote = this.notes[interval - initialNotes];
+        }
+        //interval is within array
+        else {
+            targetNote = this.notes[index + interval];
+        }
+        return targetNote;
     },
 
     populateString : function(stringName, note, extraClassString) {
@@ -68,59 +80,72 @@ Music.prototype = {
             else
                 this.populateString(elementSelector, this.strings[i - 1], '');
         }
+    }
+}
+
+const musicScales = {
+    addClassToNote : function(note, cssClass) {
+        var target = '.' + note.replace('#', '-sharp');
+        $(fretBoard1.targetElementSelector + ' ol li span' + target).addClass(cssClass);
+    },
+
+    removeHighlighting: function () {
+        $(fretBoard1.targetElementSelector + ' ol li span.highlight').removeClass('highlight');
     },
 
     getNoteViaInterval : function(note, interval) {
-        var index = this.notes.indexOf(note);
+        var index = Music.prototype.notes.indexOf(note);
 
         var targetNote;
         //interval goes off the deep end
-        if ((interval + index) >= this.notes.length) {
-            var initialNotes = this.notes.length - index;
-            targetNote = this.notes[interval - initialNotes];
+        if ((interval + index) >= Music.prototype.notes.length) {
+            var initialNotes = Music.prototype.notes.length - index;
+            targetNote = Music.prototype.notes[interval - initialNotes];
         }
         //interval is within array
         else {
-            targetNote = this.notes[index + interval];
+            targetNote = Music.prototype.notes[index + interval];
         }
         return targetNote;
     },
 
-    addClassToNote : function(note, cssClass) {
-        var target = '.' + note.replace('#', '-sharp');
-        $(this.targetElementSelector + ' ol li span' + target).addClass(cssClass);
+    showMajorScale : function(root) {
+        //WWHWWWH
+        musicScales.removeHighlighting();                                 //degree of scale
+        root = root.replace('-sharp', '#');                               //===============
+        musicScales.addClassToNote(root, 'root');                                       //1
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 2), 'inScale'); //2
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 4), 'inScale'); //3
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 5), 'inScale'); //4
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 7), 'inScale'); //5
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 9), 'inScale'); //6
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 11), 'inScale');//7
     },
 
     showMajorChordNotes : function(root) {
         //1 3 5
-        this.removeHighlighting();                          //degree of scale
-        root = root.replace('-sharp', '#');                 //===============
-        this.addClassToNote(root, 'root');                                //1
-        this.addClassToNote(this.getNoteViaInterval(root, 4), 'inScale'); //2
-        this.addClassToNote(this.getNoteViaInterval(root, 7), 'inScale'); //3
-    },
-
-    showMajorScale : function(root) {
-        //WWHWWWH
-        this.removeHighlighting();                          //degree of scale
-        root = root.replace('-sharp', '#');                 //===============
-        this.addClassToNote(root, 'root');                                //1
-        this.addClassToNote(this.getNoteViaInterval(root, 2), 'inScale'); //2
-        this.addClassToNote(this.getNoteViaInterval(root, 4), 'inScale'); //3
-        this.addClassToNote(this.getNoteViaInterval(root, 5), 'inScale'); //4
-        this.addClassToNote(this.getNoteViaInterval(root, 7), 'inScale'); //5
-        this.addClassToNote(this.getNoteViaInterval(root, 9), 'inScale'); //6
-        this.addClassToNote(this.getNoteViaInterval(root, 11), 'inScale');//7
+        musicScales.removeHighlighting();                                 //degree of scale
+        root = root.replace('-sharp', '#');                               //===============
+        musicScales.addClassToNote(root, 'root');                                       //1
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 4), 'inScale'); //2
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 7), 'inScale'); //3
     },
 
     showPentatonicScale : function(root) {
         //drop 4th and 7th
-        this.removeHighlighting();                          //degree of scale
-        root = root.replace('-sharp', '#');                 //===============
-        this.addClassToNote(root, 'root');                                //1
-        this.addClassToNote(this.getNoteViaInterval(root, 2), 'inScale'); //2
-        this.addClassToNote(this.getNoteViaInterval(root, 4), 'inScale'); //3
-        this.addClassToNote(this.getNoteViaInterval(root, 7), 'inScale'); //5
-        this.addClassToNote(this.getNoteViaInterval(root, 9), 'inScale'); //6
+        musicScales.removeHighlighting();                                 //degree of scale
+        root = root.replace('-sharp', '#');                               //===============
+        musicScales.addClassToNote(root, 'root');                                       //1
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 2), 'inScale'); //2
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 4), 'inScale'); //3
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 7), 'inScale'); //5
+        musicScales.addClassToNote(musicScales.getNoteViaInterval(root, 9), 'inScale'); //6
+    }
+
+}
+
+let fretBoard = {
+    clearFretboard : function() {
+        $(fretBoard1.targetElementSelector + ' ol').remove();
     }
 }
